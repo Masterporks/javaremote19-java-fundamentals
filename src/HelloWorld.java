3db6c449149b4600a904794daeaaaa5273b13101
Joosep Korela <joosepkorela@gmail.com>
2022-09-14 19:23:07 +0300

    HelloWorld done

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+
+* The first hello world program
+ * @author Joosep Korela
+        */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
