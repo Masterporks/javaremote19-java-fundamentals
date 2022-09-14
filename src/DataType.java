a21d9ee4a04763488923030d22b7ade0f6e3f37c
Joosep Korela <joosepkorela@gmail.com>
2022-09-14 21:02:53 +0300

    Added datatypes

--- src/DataType.java
+++ src/DataType.java
@@ -5,6 +5,8 @@
  */
 
 public class DataType {
+    //Primitive Data Types
+    // Numbers
     int a = 1;
     byte byteNumber = 120;
     long longNumber=6453738833L;
@@ -28,5 +30,44 @@ char alpha = 'F';
     char animal; // ''
     boolean isClassTomorrow; // false
 
+//Non-primitive data types
+
+    String word ="I am a developer";
+    Character random = 'u';
+    Integer number = 5;
+    Float numberSecond = 2.45F;
+    Integer randomNumber; //null
+
+
+    // Multi-declaration
+int numberOne, numberTwo, numberThree;
+String fruit = "apple", vegetable ="pumpkin";
+
+
+private static String Car = "Toyota";
+public static String Food = "Cake";
+
+
+    public static void main(String[] args) {
+        int k = 0;
+        Integer y = null;
+        System.out.println(k);
+        System.out.println(y);
+
+String _color = "black";
+
+int $hexaNumber = 0x2A;
+
+float exam12_mark = 100.10f;
+
+String className = "sda";
+
+int a = 100;
+int b = 0;
+int c = a + b;
+
+        System.out.println(Car);
+        System.out.println(Food);
+    }
 
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+
+
+        System.out.println(DataType.Food);
     }
+
+
+
 }
