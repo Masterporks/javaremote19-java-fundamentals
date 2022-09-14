761038dab47288e655137110c5b45804a8c9165b
Joosep Korela <joosepkorela@gmail.com>
2022-09-14 20:05:32 +0300

    Added datatypes

---
+++ src/DataType.java
@@ -0,0 +1,32 @@
+/**
+ * Examples of Primitive datatype
+ *
+ * @author Joosep Korela
+ */
+
+public class DataType {
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber=6453738833L;
+    short shortNumber = -15387;
+    float decimalNumber = 2.475453334366443f;
+
+//Character
+
+char alpha = 'F';
+
+//True/False
+    boolean hadlunch = true; //had lunch
+    boolean isHoliday = false;
+
+    /* Multi-line
+    comment
+     */
+
+    int i; // 0
+    float x; // 0,00f
+    char animal; // ''
+    boolean isClassTomorrow; // false
+
+
+}
