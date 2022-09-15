82796edb0f2123943ca8830e0438c607c26100ff
Joosep Korela <joosepkorela@gmail.com>
2022-09-15 21:02:19 +0300

    mostly used operator

---
+++ src/OperatorHomework.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+
+public class OperatorHomework {
+
+    public static void main(String[] args) {
+        int i = 9;
+        int a = 18;
+        int result = // your expression here
+
+        /*
+        1.
+        2.
+        3.
+        4.
+        5.
+        6
+        7
+        8.
+        sout
+
+         */
+
+    }
+
+
+}
