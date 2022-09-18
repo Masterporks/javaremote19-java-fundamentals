28534923bf66e422826188da5dec2044bd347beb
root <joosepkorela@gmail.com>
2022-09-18 20:05:23 +0300

    First Homework

--- src/OperatorHomework.java
+++ src/OperatorHomework.java
@@ -1,26 +1,29 @@
 /**
+ ** The first Homework on Operators
  *
+ *  * @author Joosep Korela
  */
 
 public class OperatorHomework {
 
     public static void main(String[] args) {
+
         int i = 9;
         int a = 18;
-        int result = // your expression here
+        int b = 7;
+        int c = 64;
+        int d = 4;
+        int result = a - b + (c / d + a) + i % d; // your expression here
 
         /*
-        1.
-        2.
-        3.
-        4.
+        1. First  i will calculate the brackets (c / d + a)
+        2. Inside brackets the first move is c / d and then we add int a // (64 / 4) = 16 + 18 = 34
+        3. Next we sum up i % d with bracket  (34)+ 9 % 4  // 34 + 1 = 35
+        4. Next we calculate left to right  18 - 7 + 35= 46
         5.
-        6
-        7
-        8.
-        sout
 
          */
+        System.out.println(result);
 
     }
 
