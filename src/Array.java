ae407ff3ad6ed6856885edf71ffc825f3793d8ad
root <joosepkorela@gmail.com>
2022-09-22 19:59:41 +0300

    Array class completed

--- src/Array.java
+++ src/Array.java
@@ -35,18 +35,18 @@ public class Array {
 
         System.out.println(veggies[1]);  //shows default value because [1] isn't defined but 3 and 4 are
 
-        String[] food = new String[] {"cake", "porridge", "rice"};
+        String[] food = new String[]{"cake", "porridge", "rice"};
 
         int[] ageArray = new int[3];
 
         //processing array
         //conventional for-loop
-        for(int i = 0; i < fruits.length; i++){ // i = index of array
+        for (int i = 0; i < fruits.length; i++) { // i = index of array
             System.out.println(fruits[i]);
         }
 
         //enhanced for-loop
-        for(String fruit: fruits) {
+        for (String fruit : fruits) {  // String:String[]    int: int[]
             System.out.println(fruit);
         }
 
@@ -55,11 +55,11 @@ public class Array {
 
         int result = b[0]; // 9
 
-        for(int x : b){
-                if(x > result){
-                    result = x;
+        for (int x : b) {
+            if (x > result) {
+                result = x;
 
-                }
+            }
         }
         System.out.println(result);
 
@@ -68,15 +68,75 @@ public class Array {
         // To find smallest element in the array(b)
         int result1 = b[1]; // 27
 
-        for(int y : b){
-            if(y < result1){
+        for (int y : b) {
+            if (y < result1) {
                 result1 = y;
 
             }
         }
         System.out.println(result1);
 
-    }
 
+        //Multi-dimensional array
+        // 2D array
+
+        //Row - 1
+        int[][] array2d = new int[3][3];
+        array2d[0][0] = 1;
+        array2d[0][1] = 0;
+        array2d[0][2] = 1;
+
+        //Row - 2
+        array2d[1][0] = 2;
+        array2d[1][1] = 10;
+        array2d[1][2] = 9;
+
+        //Row - 3
+        array2d[2][0] = 4;
+        array2d[2][1] = 9;
+        array2d[2][2] = 8;
+
+        //Can also write as a one row
+        int[][] array2dVer2 = {{1, 0, 1}, {2, 10, 9}, {4, 9, 8}};
+
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                System.out.print(array2dVer2[x][y] + " ");
+            }
+            System.out.println();
+        }
+
+        //3D array 1D + 2D array
+        //1D=> number of tables
+        //2D array => array with rows and columns
+
+        int[][][] array3d = new int[3][3][3];
+
+        //TABLE-1
+        //Row - 1
+        array3d[0][0][0] = 1;
+        array3d[0][0][1] = 0;
+        array3d[0][0][2] = 1;
+
+        int[][][] array3dVer2 = {
+                {{1, 0, 1}, {1, 8, 0}, {2, 5, 7}},
+                {{9, 8, 7}, {5, 8, 9}, {3, 5, 0}},
+                {{5, 6, 2}, {4, 5, 7}, {2, 6, 8}}
+        };
+
+        for (int w = 0; w < 3; w++) {
+            for (int z = 0; z < 3; z++) {
+                for (int q = 0; q < 3; q++) {
+
+
+                    System.out.print(array3dVer2[w][z][q] + "\t");  // \t prints space between numbers
+                }
+                System.out.println();
+
+
+            }
+            System.out.println("\n\n"); //  \n\n  prints space between each matrix
+        }
+    }
  }
 
