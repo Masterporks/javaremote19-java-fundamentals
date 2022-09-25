16d246d9b99b36409df60648e98e0135f3d6936d
root <joosepkorela@gmail.com>
2022-09-25 20:21:38 +0300

    homework 2 done

--- src/Homework2.java
+++ src/Homework2.java
@@ -5,28 +5,16 @@
 public class Homework2 {
     public static void main(String[] args) {
 
-        int x = 15;
-        int y = 25;
+        String  x = "15";
+        String y = "25";
+        String temp;
 
-        String firstColumn = "Black bucket";
-        String secondColumn = "White bucket";
-        String thirdColumn = "Black bucket";
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x: " + x);
+        System.out.println(("y: " + y));
 
-        System.out.println("There are 3 columns: First column is black bucket, second column is white bucket , third column is black bucket");
-        System.out.println("Nr 15 is in first column, Nr 25 is in third column");
-
-        if (x < y && firstColumn == thirdColumn);
-
-        System.out.println("Nr 15 moves from black bucket at first column to white bucket in second column");
-
-
-        if ( x < y && secondColumn == thirdColumn);
-            System.out.println("Nr 25 moves from third column black bucket to first Column black bucket");
-        if ( y > x && firstColumn == secondColumn);
-
-        System.out.println("Nr 15 moves  to third column black bucket");
-        System.out.println("Nr 25 is in first column black bucket & Nr 15 is in third column black bucket");
-        System.out.println("White bucket is empty");
     }
 
 }
